18e577c9747e7b9370a3aaec05db8ff571e11d21
William Harris <willmharris0@gmail.com>
2020-01-25 23:20:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,27 +29,25 @@ def movie_with_director_name(director_name, movie_data)
     :director_name => director_name
   }
 end
+# transforms the data for an individual movie to include the director name
 
-
-# Your code after this point
+# Your code after this point ############################################
 
 def movies_with_director_key(name, movies_collection)
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
-  #
+  final_array1 = []
+  counter1 = 0 
+    while counter1 < movies_collection.length do
+      
+      counter1 += 1 
+    end 
+  final_array1
+end
+ # GOAL: For each Hash in an Array (movies_collection), provide a collection of movies and a directors name to the movie_with_director_name method and accumulate the returned Array of movies into a new Array that's returned by this method.
   # INPUT:
   # * name: A director's name
   # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
   # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
-end
-
+  # Array of Hashes where each Hash represents a movie; however, they should all have a :director_name key. This addition can be done by using the provided movie_with_director_name method
 
 def gross_per_studio(collection)
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
